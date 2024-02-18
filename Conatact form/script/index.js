d7be2358d993a990893f764215e1a0eb3ea204c3
umair kifayat <umairkifayat3@gmail.com>
2024-02-18 02:03:12 -0800

    completed

--- Conatact form/script/index.js
+++ Conatact form/script/index.js
@@ -1,4 +1,4 @@
 // Auto redirect to next page after 5 seconds
 setTimeout(function() {
-    window.location.href = "academic.html";
+    window.location.href = "tech-interest.html";
 }, 5000); // Redirect to tech-interest.html after 5 seconds
