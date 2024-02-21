df7575dbe7912b431e97e2303a230b4c5b531d39
umair kifayat <umairkifayat3@gmail.com>
2024-02-20 23:35:33 -0800

    'updated'

---
+++ internee website/scripts/app.js
@@ -0,0 +1,18 @@
+// COUNTER
+let valueDisplays = document.querySelectorAll(".counter_number");
+let interval = 5000;
+
+valueDisplays.forEach((valueDisplay) => {
+    let startValue = 0;
+    let endValue = parseInt(valueDisplay.getAttribute("data-val"));
+    let duration = Math.floor(interval / endValue);
+    let counter = setInterval(function () {
+        startValue += 1;
+        valueDisplay.textContent = startValue;
+        if(startValue == endValue){
+            clearInterval(counter)
+        }
+    },duration)
+
+
+});
